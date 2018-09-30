462477efb4fcac8c1cf800e64e92bd13d9018c9f
SANGET <3348592+SANGET@users.noreply.github.com>
2018-09-30 23:07:34 +0800

    update test

--- __test__/url-resolve.test.js
+++ __test__/url-resolve.test.js
@@ -1,9 +1,12 @@
 import {
-  resolveUrl
+  resolveUrl, toBase64Str
 } from '../src/url-resolve';
 
 describe('Testing url resolver', () => {
   test('resolveUrl ["http://abc.com", "path1", "path2" to "http://abc.com/path1/path2"]', () => {
     expect(resolveUrl("http://abc.com", "path1", "path2")).toMatch('http://abc.com/path1/path2');
+  });
+  test('toBase64Str 123 to MTIz', () => {
+    expect(toBase64Str(123)).toMatch('MTIz');
   });
 });
