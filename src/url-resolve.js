5da26b1d4a32bf443b94aeb664930d3c91ab06a1
SANGET <AlexZhang@Alexs-iMac.local>
2018-10-30 17:40:03 +0800

    update

--- src/url-resolve.js
+++ src/url-resolve.js
@@ -22,7 +22,6 @@ export function searchUrlParams(searchStr) {
   return window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURI(searchStr).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1");
 }
 
-// TODO: 因为写法是 //，所以出现问题
 export function resolveUrl(baseUrl, ...paths) {
   if(!baseUrl) return console.log('please pass baseUrl');
   baseUrl = baseUrl.replace(/\/+$/, '');
