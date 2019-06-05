c14acc31eae1c4ff5150826eb5b2b89c80d8b562
SANGET <AlexZhang@Alexs-iMac.local>
2019-06-05 11:20:02 +0800

    添加 docz 文档配置

---
+++ doczrc.js
@@ -0,0 +1,42 @@
+import doczPluginNetlify from "docz-plugin-netlify";
+import { css } from 'docz-plugin-css';
+import themeConfig from './docz/theme-config/config';
+
+export default {
+  // dest: 'docz-dist',
+  title: 'Uke Request',
+  description: '通讯加密库',
+  indexHtml: 'docz/index.html',
+  wrapper: 'docz/wrapper',
+  codeSandbox: false,
+  hashRouter: true,
+  files: '**/*.mdx',
+  htmlContext: {
+    head: {
+      links: [{
+        rel: 'stylesheet',
+        // href: 'https://codemirror.net/theme/dracula.css'
+        href: 'https://codemirror.net/theme/mdn-like.css'
+      }],
+    },
+  },
+  themeConfig: themeConfig,
+  menu: [
+    'Getting Started / 开始',
+    'URL Resolver / URL解析器',
+  ],
+  // modifyBundlerConfig: (config) => {
+  //   config.resolve.extensions.push('.scss');
+  //   config.module.rules.push({
+  //     test: /\.scss$/,
+  //     use: ["style-loader", "css-loader", "sass-loader"]
+  //   });
+  //   return config;
+  // },
+  plugins: [
+    doczPluginNetlify(),
+    css({
+      preprocessor: 'sass',
+    })
+  ]
+};
