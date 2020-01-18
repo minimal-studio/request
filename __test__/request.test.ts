816de8fe132055256a2b3ff1a91d32a9a8d73c19
sanget <zh.sanget@gmail.com>
2020-01-18 13:46:32 +0800

    Add test

---
+++ __test__/request.test.ts
@@ -0,0 +1,39 @@
+import {
+  RequestClass
+} from '../src/request';
+
+const getClientFingerprint = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('sdhuihqwirhqowirh');
+    }, 300)
+  })
+}
+
+describe('Testing RESTFul', () => {
+  test('Get items', (done) => {
+    const $R = new RequestClass();
+    const setFPHeader = async (data) => {
+      const fingerprint = await getClientFingerprint();
+      $R.setConfig({
+        commonHeaders: {
+          FP: fingerprint
+        }
+      });
+      return data;
+    };
+    
+    $R.useBefore(setFPHeader);
+
+    $R.get({
+      url: '/test',
+      params: {
+        id: '123'
+      }
+    })
+    .then((resData) => {
+      expect(resData).toBe('peanut butter');
+      done();
+    })
+  });
+});
